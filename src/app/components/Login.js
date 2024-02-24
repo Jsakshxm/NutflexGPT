d22ee2f78fc8b7b186aa3ec440d572bcd0c51209
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-02-24 23:05:10 +0530

    Update Login.js

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import { FormDataValidate } from '../utils/validate'
 import { useEffect, useRef, useState } from 'react'
 import { auth } from '../utils/firebase';
-import { useRouter } from 'next/Navigation';
+import { useRouter } from 'next/navigation';
 
 
 
