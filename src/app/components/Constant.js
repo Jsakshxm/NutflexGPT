616cf0442016d45ca7deb7f1921afe5d9218864c
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-10-20 16:59:16 +0530

    verification

--- src/app/components/Constant.js
+++ src/app/components/Constant.js
@@ -1,4 +1,4 @@
-export const bg = "https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+export const bg = "/bg.jpg";
 export const profile = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
 export const API_KEY ="c839e9aab551695122aa5df01df90f47"
 export const ACCESS_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODM5ZTlhYWI1NTE2OTUxMjJhYTVkZjAxZGY5MGY0NyIsInN1YiI6IjY2MDY5YWZmNTkwMDg2MDE3Y2I3MWUwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hD6LROHwcNjgBOGQmjf8pdyMHn1H1Q1ao9TGXbZRBwg"
